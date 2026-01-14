bcb998eb80cdcc486e8706b71da0e99712d507b3
ParkHyoungChul <asdhk0762@gmail.com>
2026-01-14 11:01:20 +0900

    update model for ds6

--- DL/DS6_MLOps/MLOps2_serving/streamlit_app.py
+++ DL/DS6_MLOps/MLOps2_serving/streamlit_app.py
@@ -8,5 +8,5 @@ p_l = st.slider("꽃잎 길이", 0.0, 7.0, 1.5)
 p_w = st.slider("꽃잎 너비", 0.0, 2.5, 0.2)
 
 if st.button("예측하기"):
-    res = requests.post("http://34.69.160.60/predict", json={"data": [s_l, s_w, p_l, p_w]})
+    res = requests.post("http://34.170.213.69//predict", json={"data": [s_l, s_w, p_l, p_w]})
     st.success(f"예측된 클래스 번호: {res.json()['class_index']}")
